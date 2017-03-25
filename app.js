45054e2c22c2b3abddabb067c10f640115cd40fd
Hoàng Đức Thắng <thanghoang07@gmail.com>
2017-03-25 18:34:15 +0700

    Initial commit

--- app.js
+++ app.js
@@ -23,7 +23,18 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', [
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.onDefault([
     function (session, args, next) {
         if (!session.userData.name) {
             session.beginDialog('/profile');
